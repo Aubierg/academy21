99905f4363d197c9dc8ddb3def7484b5136d0e7b
Aubierge <ndjiyaaubierge@gmail.com>
2026-06-10 14:43:54 +0100

    chore: remove academy21-frontend folder from backend repo

--- academy21-frontend/.next/dev/static/chunks/src_app_formations_page_tsx_1deh3zh._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_formations_page_tsx_1f7rd7k._.js"],
-    source: "dynamic"
-});
